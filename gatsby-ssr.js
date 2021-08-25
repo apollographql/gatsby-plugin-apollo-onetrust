832ddd02ec6689c6a91f2fb07dffb2c494212688
Jay Chesterman <jay.chesterman@live.ca>
2021-08-25 17:16:33 -0400

    Init push

---
+++ gatsby-ssr.js
@@ -0,0 +1,13 @@
+export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+  setHeadComponents([
+    <script
+      src={pluginOptions.autoBlockSrc} />,
+    <script src={pluginOptions.otSDKStubSrc}
+      type="text/javascript" charset="UTF-8" data-domain-script={pluginOptions.dataDomainScript} />,
+    <script type="text/javascript" dangerouslySetInnerHTML={{
+      __html: `
+        function OptanonWrapper() {}
+      `
+    }} />,
+  ])
+}

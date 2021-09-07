2de73c6176dbbf83f7135e9194a7273bbb9123c3
Trevor Blades <tdblades@gmail.com>
2021-09-07 12:38:52 -0700

    Format code

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+export const onRenderBody = ({setHeadComponents}, pluginOptions) => {
   if (process.env.NODE_ENV === 'production' && !pluginOptions.skip) {
     setHeadComponents([
+      <script src={pluginOptions.autoBlockSrc} />,
       <script
-        src={pluginOptions.autoBlockSrc} />,
-      <script src={pluginOptions.otSDKStubSrc}
-        type="text/javascript" charset="UTF-8" data-domain-script={pluginOptions.dataDomainScript} />,
-      <script type="text/javascript" dangerouslySetInnerHTML={{
-        __html: `
-          function OptanonWrapper() {}
-        `
-      }} />,
+        src={pluginOptions.otSDKStubSrc}
+        type="text/javascript"
+        charset="UTF-8"
+        data-domain-script={pluginOptions.dataDomainScript}
+      />,
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{__html: 'function OptanonWrapper() {}'}}
+      />,
     ])
   }
 }

41d95850b1c389b52e2632e0cccaa41941a33e10
Jay Chesterman <jay.chesterman@live.ca>
2021-09-07 13:27:56 -0400

    Add skip option & context production condition

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  setHeadComponents([
-    <script
-      src={pluginOptions.autoBlockSrc} />,
-    <script src={pluginOptions.otSDKStubSrc}
-      type="text/javascript" charset="UTF-8" data-domain-script={pluginOptions.dataDomainScript} />,
-    <script type="text/javascript" dangerouslySetInnerHTML={{
-      __html: `
-        function OptanonWrapper() {}
-      `
-    }} />,
-  ])
+  if (process.env.NODE_ENV === 'production' && !pluginOptions.skip) {
+    setHeadComponents([
+      <script
+        src={pluginOptions.autoBlockSrc} />,
+      <script src={pluginOptions.otSDKStubSrc}
+        type="text/javascript" charset="UTF-8" data-domain-script={pluginOptions.dataDomainScript} />,
+      <script type="text/javascript" dangerouslySetInnerHTML={{
+        __html: `
+          function OptanonWrapper() {}
+        `
+      }} />,
+    ])
+  }
 }
